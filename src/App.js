16da3cb25638c7fc98fd3b78fdeb234f25ef84f2
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-11-07 18:11:31 -0500

    deleted logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+
+    super(props)
+
+    this.state  = {
+      board : Array(9).fill(null)
+    }
+  }
+  handleClick(index){
+    
+    // this.setstate( {
+    //   board
+    // }).board[index] = "x"
+    // console.log(this.state.board)
+    
+    // console.log(index)
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    const Box = this.state.board.map(
+      (box, index)  => 
+      <div className="box" 
+      key={index}
+      onClick={() => this.handleClick(index)}>
+      {box}
       </div>
+      )
+
+    return (
+      <div className="container">
+        <h1>Tic Tac Toe</h1>
+        <div className="board">
+        {Box}
+      </div>
+      </div>
+      
     );
   }
 }
